df579bd1b0282f5666ba38a3461da9440ac313ed
hnishi <hnishi>
2017-08-22 17:48:42 +0900

    v7.1

--- do1_pmf2d.py
+++ do1_pmf2d.py
@@ -1,2 +1,1 @@
-#python plot3_pmf2d.py  helix_count.dat gyrate.dat > plot3_pmf2d_hc_rg.dat
-python pmfnishi2d.py  ./helix_count_1_10.dat ./area_1_10.dat > out_pmfnishi.dat
+python pmfnishi2d.py  > out_pmfnishi2d.dat
